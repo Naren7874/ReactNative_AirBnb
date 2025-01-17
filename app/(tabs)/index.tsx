23ce9817876238f5416159b79cb5979f455d02d3
Narendra <malinarendra7874@gmail.com>
2025-01-18 01:18:06 +0530

    header 50% work done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { Link } from 'expo-router'
+import { Link, Stack } from 'expo-router'
+import ExploreHeader from '@/components/ExploreHeader'
+import Listing from '@/components/Listing'
 
 const index = () => {
   return (
-    <View>
-      <Link href={'/(modals)/login'}>Login</Link>
+    <View style={{ flex: 1}}>
+      <Stack.Screen options={{
+        header:()=><ExploreHeader/>
+      }}/>
+      <Listing/>
+      {/* <Link href={'/(modals)/login'}>Login</Link>
       <Link href={'/(modals)/booking'}>booking</Link>
-      <Link href={'/listing/13546'}>Listing Details</Link>
+      <Link href={'/listing/13546'}>Listing Details</Link> */}
     </View>
   )
 }
